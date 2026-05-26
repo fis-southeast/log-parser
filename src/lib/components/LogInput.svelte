9014cf4491b7735f96d2eb8b38f866547be2a9a7
maxpeterkaya <git@maxkaya.com>
2026-05-26 14:30:56 -0400

    feat: add local error state

--- src/lib/components/LogInput.svelte
+++ src/lib/components/LogInput.svelte
@@ -9,10 +9,11 @@
 		onSubmit: () => void;
 	} = $props();
 
+	let error = $state('');
 	let textarea: HTMLTextAreaElement;
-	let canSubmit = $derived(logs.trim().length > 0 && !isLoading);
+	let canSubmit = $derived(logs.trim().length > 0 && !isLoading && error.length == 0);
 
-	let files = $state({} as FileList);
+	let files: FileList | undefined = $state();
 
 	function resizeInput() {
 		if (!textarea) return;
@@ -31,21 +32,27 @@
 	$effect(() => {
 		if (files) {
 			// Note that `files` is of type `FileList`, not an Array:
-			// https://developer.mozilla.org/en-US/docs/Web/API/FileLis
+			// https://developer.mozilla.org/en-US/docs/Web/API/FileList
 
 			if (files.length === 0) {
-				logs = ""
-				return
+				logs = '';
+				return;
 			}
 
 			if (files.length > 1) {
-				logs = ""
-				return
+				logs = '';
+				return;
 			}
 
 			for (const f of files) {
+				if (f.size > 50 * 1024 * 1024) {
+					error = 'File too large!';
+					files = {} as FileList;
+					return;
+				}
+				error = '';
 				console.log(`${f.name}: ${f.size} bytes`);
-				f.text().then((v) => logs = v)
+				f.text().then((v) => (logs = v));
 			}
 		}
 	});
@@ -57,7 +64,14 @@
 	>
 		<div class="relative">
 			<label class="sr-only" for="logs">Paste your logs</label>
-			<input id="log-file" class="sr-only" type="file" disabled={isLoading} bind:files accept=".txt, text/plain, .log, .logs" />
+			<input
+				id="log-file"
+				class="sr-only"
+				type="file"
+				disabled={isLoading}
+				bind:files
+				accept=".txt, text/plain, .log, .logs"
+			/>
 			{#if !logs}
 				<div
 					class="pointer-events-none absolute top-5 left-5 z-10 text-base leading-7 text-zinc-600"
@@ -84,6 +98,12 @@
 				autofocus
 			></textarea>
 		</div>
+
+		{#if error.length > 0}
+			<div class="flex gap-3 border-t border-white/10 px-4 py-4 text-lg text-red-300">
+				{error}
+			</div>
+		{/if}
 
 		<div
 			class="flex flex-col gap-3 border-t border-white/10 px-4 py-4 sm:flex-row sm:items-center sm:justify-between"
